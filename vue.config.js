6407ca544645adafa500f4293683bbd2e5352709
Abdi-El <abdila1099@gmail.com>
2022-10-20 22:27:59 +0200

    fix sccs loader nom error

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
 });
 module.exports = {
   pluginOptions: {
     "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
-    }
-  }
+      preProcessor: "stylus",
+      patterns: [path.resolve(__dirname, "./src/styles/global.scss")],
+    },
+  },
 };
